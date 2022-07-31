3d4b1234addfd7bf7a81ec1753125ef0b819d588
Eduardo V. Lemes <91350022+eduardovlemes@users.noreply.github.com>
2022-07-31 13:10:33 -0300

    chore: exercício 5

---
+++ exercícios/[M1S05]_ex05/index.js
@@ -0,0 +1,4 @@
+//Utilize o if ternário para construir uma função que recebe o número do dias e retorna se pertence a primeira ou a segunda quinzena
+const descobreQuinzena = numeroDoDia = numeroDoDia <= 15 ? "primeira quinzena" : "segunda quinzena"
+  
+  console.log(descobreQuinzena(1))
